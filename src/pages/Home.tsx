3142ff7d1c57aff73b367a21725dc148cf1d70b6
Quentin Legot <legotquentin@gmail.com>
2024-09-02 23:44:47 +0200

    Some ajustements correpsonding to my situation and typo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,13 +27,13 @@ export default function Home() {
     return (
     <div>
         <CvTitle name={t('cv.section.experience')} cards={[
-            {title: t('cv.experience.0.title'), icon: OB_company, company: "Orange Business", date_start: new Date('4 March 2024'), type: t('cv.experience.0.type'), description:(<p>{t('cv.experience.0.description')}</p>)},
+            {title: t('cv.experience.0.title'), icon: OB_company, company: "Orange Business", date_start: new Date('4 March 2024'), date_end: new Date("31 august 2024"), type: t('cv.experience.0.type'), description:(<p>{t('cv.experience.0.description')}</p>)},
             {title: t('cv.experience.1.title'), icon: OB_company, company: "Orange Business", date_start: new Date('1 May 2023'), date_end: new Date("31 august 2023"), type: t('cv.experience.1.type'), description: <p>{t('cv.experience.1.description')}</p>},
             {title: t('cv.experience.2.title'), icon: greyc_company, company: "Greyc", date_start: new Date('1 May 2022'), date_end: new Date('31 june 2022'), type: t('cv.experience.2.type'), description: <p>{t('cv.experience.2.description')}</p>}
         ]} />
         <CvTitle name={t('cv.section.formation')} cards={[
             { title: t('cv.formation.0.title'), icon: rennes_univ, company: "Université de Rennes", date_start: new Date('1 september 2022'), date_end: new Date('31 August 2024'), description: <p>{t('cv.formation.0.description')}</p>},
-            { title: t("cv.formation.1.title"), icon: caen_univ, company: "Univerisité de Caen Normandie", date_start: new Date("1 september 2019"), date_end: new Date("31 june 2022")}
+            { title: t("cv.formation.1.title"), icon: caen_univ, company: "Université de Caen Normandie", date_start: new Date("1 september 2019"), date_end: new Date("31 june 2022")}
         ]} />
         <CvTitle name={t('cv.section.skills')} cards={[]}/>
 
